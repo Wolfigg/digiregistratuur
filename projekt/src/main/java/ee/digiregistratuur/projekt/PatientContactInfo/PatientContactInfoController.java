4c0a1a5334a462c14b73d297b50bedcb77bff3ad
Katrin Valge <katrin.valge@mail.ee>
2022-02-15 14:47:37 +0200

    patient ja patientContactInfo first controllers updated paths

--- projekt/src/main/java/ee/digiregistratuur/projekt/PatientContactInfo/PatientContactInfoController.java
+++ projekt/src/main/java/ee/digiregistratuur/projekt/PatientContactInfo/PatientContactInfoController.java
@@ -3,19 +3,21 @@ package ee.digiregistratuur.projekt.PatientContactInfo;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping("/patient/contact")
 
 public class PatientContactInfoController {
 
     @Resource
     private PatientContactInfoService patientContactInfoService;
 
-    @GetMapping("/get/patient/contact/info/by/idcode")
+    @GetMapping("/get/by/idcode")
     @Operation(summary = "Leiab kasutaja kontaktandmed isikukoodi järgi")
     public PatientContactInfoDto findPatientContactInfoById(@RequestParam String idCode) {
         PatientContactInfoDto contactInfoById = patientContactInfoService.findPatientContactInfoById(idCode);
--- projekt/src/main/java/ee/digiregistratuur/projekt/patient/PatientController.java
+++ projekt/src/main/java/ee/digiregistratuur/projekt/patient/PatientController.java
@@ -3,19 +3,21 @@ package ee.digiregistratuur.projekt.patient;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
 @RestController
+@RequestMapping("/patient")
 
 public class PatientController {
 
     @Resource
     private PatientService patientService;
 
-    @GetMapping("/get/idCode")
+    @GetMapping("/get/by/idCode")
     @Operation(summary = "Leiab kasutaja isikukoodi järgi")
     public void findPatientByIdCode(@RequestParam String idCode) {
         patientService.findPatientByIdCode(idCode);
